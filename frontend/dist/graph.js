ad18ef94186fea6357ceed65cd69329698d71fc9
cdrik6 <cdrik6@free.fr>
2025-10-30 02:38:56 +0100

    graph

--- frontend/dist/graph.js
+++ frontend/dist/graph.js
@@ -1,11 +1,13 @@
+const scatterContainer = document.getElementById("scatter");
+
 // Scatter
 const canvasScatter = document.createElement("canvas");	
 canvasScatter.width = 200;
 canvasScatter.height = 200;
-statsContainer.appendChild(canvasScatter);		
+scatterContainer.appendChild(canvasScatter);		
 const ctxScatter = canvasScatter.getContext("2d");
 if (!ctxScatter) throw new Error("Cannot get Scatter canvas context");
-fetch(`/api/stats/scatter`, { method: 'GET', credentials: 'include' })
+fetch(`/api/data/scatter`, { method: 'GET' })
 .then(res => {
     if (!res.ok) { throw new Error(`HTTP error status: ${res.status}`); }
     return (res.json());
@@ -16,20 +18,20 @@ fetch(`/api/stats/scatter`, { method: 'GET', credentials: 'include' })
         {
             type: 'scatter',
             data: {					
-                datasets: [{ label: 'Spread vs Max Touch', data: data }]
+                datasets: [{ label: 'km vs price', data: data }]
             },
             options: {					
                 responsive: true,
                 maintainAspectRatio: true,
                 plugins: {
                     legend: { display: false },
-                    title: { display: true,	text: 'Spread vs Max Touch'	}
+                    title: { display: true,	text: 'km vs price'	}
                 },
                 scales: {						
-                    x: { title: { display: true, text: 'Spread' } },
-                    y: { title: { display: true, text: 'Max Touch' } }
+                    x: { title: { display: true, text: 'km' } },
+                    y: { title: { display: true, text: 'price' } }
                 }
             }
         });		
 })
-.catch(err => { console.error("Bar chart failed: ", err); });	
+.catch(err => { console.error("Sactter chart failed: ", err); });
